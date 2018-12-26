f8aeeb35f9d39b994c6914b96926b95457a5cf9e
Rozi <43922712+rr4tune@users.noreply.github.com>
2018-12-26 11:41:59 +0300

    Add files via upload

--- MaxiPlast/Maxi Plast (NEW)/sendemail.php
+++ MaxiPlast/Maxi Plast (NEW)/sendemail.php
@@ -2,7 +2,7 @@
 	header('Content-type: application/json');
 	$status = array(
 		'type'=>'submit',
-		'message'=>'Thank you for contact us. As early as possible  we will contact you '
+		'message'=>'Спасибо'
 	);
 
     $name = @trim(stripslashes($_POST['name'])); 
